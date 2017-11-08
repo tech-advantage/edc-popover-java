195ebdb84e26cab956240022aeb6a6589ba41c50
Gregory COCHON <Gregory.COCHON@tech-advantage.com>
2017-11-08 18:40:35 +0100

    Upgrade the client version and add logger in internal browser to display the console output.

--- src/main/java/fr/techad/edc/popover/internal/browser/SwingBrowser.java
+++ src/main/java/fr/techad/edc/popover/internal/browser/SwingBrowser.java
@@ -1,26 +1,25 @@
 package fr.techad.edc.popover.internal.browser;
 
+import com.sun.javafx.webkit.WebConsoleListener;
 import fr.techad.edc.popover.browser.Browser;
 import javafx.application.Platform;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
+import javafx.concurrent.Worker.State;
 import javafx.embed.swing.JFXPanel;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.web.WebEngine;
-import javafx.scene.web.WebEvent;
 import javafx.scene.web.WebView;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-import static javafx.concurrent.Worker.State.FAILED;
-
 public class SwingBrowser extends JFrame implements Browser {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SwingBrowser.class);
 
     private final JFXPanel jfxPanel = new JFXPanel();
     private final JPanel panel = new JPanel(new BorderLayout());
@@ -46,12 +45,7 @@ public class SwingBrowser extends JFrame implements Browser {
     private void initComponents() {
         createScene();
 
-        ActionListener al = new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                loadURL(txtURL.getText());
-            }
-        };
+        ActionListener al = e -> loadURL(txtURL.getText());
 
         btnGo.addActionListener(al);
         txtURL.addActionListener(al);
@@ -81,86 +75,33 @@ public class SwingBrowser extends JFrame implements Browser {
 
     private void createScene() {
 
-        Platform.runLater(new Runnable() {
-            @Override
-            public void run() {
+        Platform.runLater(() -> {
+            WebView view = new WebView();
+            engine = view.getEngine();
 
-                WebView view = new WebView();
-                engine = view.getEngine();
+            engine.titleProperty().addListener((observable, oldValue, newValue) -> SwingUtilities.invokeLater(() -> SwingBrowser.this.setTitle(newValue)));
 
-                engine.titleProperty().addListener(new ChangeListener<String>() {
-                    @Override
-                    public void changed(ObservableValue<? extends String> observable, String oldValue, final String newValue) {
-                        SwingUtilities.invokeLater(new Runnable() {
-                            @Override
-                            public void run() {
-                                SwingBrowser.this.setTitle(newValue);
-                            }
-                        });
-                    }
-                });
-
-                engine.setOnStatusChanged(new EventHandler<WebEvent<String>>() {
-                    @Override
-                    public void handle(final WebEvent<String> event) {
-                        SwingUtilities.invokeLater(new Runnable() {
-                            @Override
-                            public void run() {
-                                lblStatus.setText(event.getData());
-                            }
-                        });
-                    }
-                });
-
-                engine.locationProperty().addListener(new ChangeListener<String>() {
-                    @Override
-                    public void changed(ObservableValue<? extends String> ov, String oldValue, final String newValue) {
-                        SwingUtilities.invokeLater(new Runnable() {
-                            @Override
-                            public void run() {
-                                txtURL.setText(newValue);
-                            }
-                        });
-                    }
-                });
-
-                engine.getLoadWorker().workDoneProperty().addListener(new ChangeListener<Number>() {
-                    @Override
-                    public void changed(ObservableValue<? extends Number> observableValue, Number oldValue, final Number newValue) {
-                        SwingUtilities.invokeLater(new Runnable() {
-                            @Override
-                            public void run() {
-                                progressBar.setValue(newValue.intValue());
-                            }
-                        });
-                    }
-                });
-
-                engine.getLoadWorker()
-                        .exceptionProperty()
-                        .addListener(new ChangeListener<Throwable>() {
-
-                            @Override
-                            public void changed(ObservableValue<? extends Throwable> o, Throwable old, final Throwable value) {
-                                if (engine.getLoadWorker().getState() == FAILED) {
-                                    SwingUtilities.invokeLater(new Runnable() {
-                                        @Override
-                                        public void run() {
-                                            JOptionPane.showMessageDialog(
-                                                    panel,
-                                                    (value != null)
-                                                            ? engine.getLocation() + "\n" + value.getMessage()
-                                                            : engine.getLocation() + "\nUnexpected error.",
-                                                    "Loading error...",
-                                                    JOptionPane.ERROR_MESSAGE);
-                                        }
-                                    });
-                                }
-                            }
-                        });
-
-                jfxPanel.setScene(new Scene(view));
-            }
+            engine.setOnStatusChanged(event -> SwingUtilities.invokeLater(() -> lblStatus.setText(event.getData())));
+            engine.locationProperty().addListener((ov, oldValue, newValue) -> SwingUtilities.invokeLater(() -> txtURL.setText(newValue)));
+            engine.getLoadWorker().workDoneProperty().addListener((observableValue, oldValue, newValue) -> SwingUtilities.invokeLater(() -> progressBar.setValue(newValue.intValue())));
+            engine.getLoadWorker().exceptionProperty()
+                    .addListener((o, old, value) -> {
+                        LOGGER.debug("Exception during rendering: {}, {}, {}", o, old, value);
+                        if (engine.getLoadWorker().getState() == State.FAILED) {
+                            SwingUtilities.invokeLater(() ->
+                                    JOptionPane.showMessageDialog(
+                                            panel,
+                                            (value != null)
+                                                    ? engine.getLocation() + "\n" + value.getMessage()
+                                                    : engine.getLocation() + "\nUnexpected error.",
+                                            "Loading error...",
+                                            JOptionPane.ERROR_MESSAGE)
+                            );
+                        }
+                    });
+            // Log the error display in the console.
+            WebConsoleListener.setDefaultListener((webView, message, lineNumber, sourceId) -> LOGGER.info("Console: [{} : {}] {}", sourceId, lineNumber, message));
+            jfxPanel.setScene(new Scene(view));
         });
     }
 
