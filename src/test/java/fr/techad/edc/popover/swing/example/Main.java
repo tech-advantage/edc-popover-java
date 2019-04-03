b5f604b4429899d406bf8e009667f81cae1be9e5
Gregory COCHIN <Gregory.COCHIN@tech-advantage.com>
2019-04-03 14:39:38 +0200

    Improve the example.
    Add the MouseListener example to get the popover without the icon.

--- src/test/java/fr/techad/edc/popover/swing/example/Main.java
+++ src/test/java/fr/techad/edc/popover/swing/example/Main.java
@@ -53,23 +53,55 @@ public class Main {
         EdcSwingHelpSingleton.getInstance().setAutoDisabledMode(true);
         EdcSwingHelpSingleton.getInstance().setBrowserSize(1600, 900);
 
+        /* Main wndow */
         JFrame f = new JFrame();
+        f.setLayout(new BorderLayout());
+
+        /* Top Panel to expose the icon help */
+        JPanel helpIconPanel = new JPanel();
         FlowLayout layout = new FlowLayout();
         layout.setAlignment(FlowLayout.TRAILING);
-        f.setLayout(layout);
+        helpIconPanel.setLayout(layout);
+        f.add(helpIconPanel, BorderLayout.NORTH);
 
+        helpIconPanel.add(EdcSwingHelpSingleton.getInstance().createComponent("fr.techad.edc", "help.center"));
+        helpIconPanel.add(EdcSwingHelpSingleton.getInstance().createComponent("fr.techad.edc.configuration", "storehouses"));
+        helpIconPanel.add(EdcSwingHelpSingleton.getInstance().createComponent("fr.techad.edc.configuration", "storehouses2"));
+        helpIconPanel.add(EdcSwingHelpSingleton.getInstance().createComponent("fr.techad.edc", "help.center", "icons/icon2-32px.png"));
+        helpIconPanel.setPreferredSize(new Dimension(400, 400));
 
-        // Create the button
+        /* Bottom panel to expose mouse listener management */
+        JPanel buttonPanel = new JPanel();
+
+        /* Button Help */
+        JButton btn = new JButton("Help Info");
+        buttonPanel.add(btn);
+        btn.addMouseListener(EdcSwingHelpSingleton.getInstance().getMouseListener("fr.techad.edc", "help.center"));
+
+        /* Jlabel Help */
+        JLabel label = new JLabel("JLabel: Click here for help");
+        buttonPanel.add(label);
+        label.addMouseListener(EdcSwingHelpSingleton.getInstance().getMouseListener("fr.techad.edc.configuration", "storehouses"));
+
+        /* Button as link */
+        JButton link = new JButton();
+        link.setText("<HTML><FONT color=\"#000099\"><U>Storehouse Help</U></FONT></HTML>");
+        link.setHorizontalAlignment(SwingConstants.LEFT);
+        link.setBorderPainted(false);
+        link.setContentAreaFilled(false);
+        link.setOpaque(false);
+        link.addMouseListener(EdcSwingHelpSingleton.getInstance().getMouseListener("fr.techad.edc.configuration", "storehouses"));
+        link.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        buttonPanel.add(link);
+
+        f.add(buttonPanel, BorderLayout.SOUTH);
+
         try {
             EdcSwingHelpSingleton.getInstance().getEdcClient().loadContext();
         } catch (IOException | InvalidUrlException e) {
             e.printStackTrace();
         }
-        f.add(EdcSwingHelpSingleton.getInstance().createComponent("fr.techad.edc", "help.center"));
-        f.add(EdcSwingHelpSingleton.getInstance().createComponent("fr.techad.edc.configuration", "storehouses"));
-        f.add(EdcSwingHelpSingleton.getInstance().createComponent("fr.techad.edc.configuration", "storehouses2"));
-        f.add(EdcSwingHelpSingleton.getInstance().createComponent("fr.techad.edc", "help.center", "icons/icon2-32px.png"));
-        f.setPreferredSize(new Dimension(400, 400));
+
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.pack();
         f.setVisible(true);
