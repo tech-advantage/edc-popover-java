5967b84edaf19c715d8cf4460f719ab5bf62739e
bracq <florianbracq42@gmail.com>
2022-03-04 17:06:50 +0100

    Run app with url POST request

--- src/main/java/fr/techad/edc/popover/utils/OpenUrlAction.java
+++ src/main/java/fr/techad/edc/popover/utils/OpenUrlAction.java
@@ -8,13 +8,10 @@ import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import java.awt.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.net.HttpURLConnection;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.util.Arrays;
 
 /**
  * TECH ADVANTAGE
@@ -35,6 +32,57 @@ public class OpenUrlAction {
         this.helpConfiguration = helpConfiguration;
     }
 
+    public static void setTimeout(Runnable runnable, int delay, String url){
+        new Thread(() -> {
+            try {
+                Thread.sleep(delay);
+
+                runnable.run();
+
+                String viewerUrl = "http://localhost:3000/viewerurl";
+                URL obj = new URL(viewerUrl);
+                HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+                // Setting basic post request
+                con.setRequestMethod("POST");
+
+                con.setRequestProperty("Accept", "application/json");
+                con.setRequestProperty("Content-Type","application/json");
+
+                String input = "{\"url\": \""+url+"\"}";
+
+                // Send post request
+                con.setDoOutput(true);
+                DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+                wr.writeBytes(input);
+                wr.flush();
+                wr.close();
+
+                int responseCode = con.getResponseCode();
+                LOGGER.debug("Sending 'POST' request to URL : " + url);
+                LOGGER.debug("Post Data : " + input);
+                LOGGER.debug("Response Code : " + responseCode);
+
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(con.getInputStream()));
+                String output;
+                StringBuffer response = new StringBuffer();
+
+                while ((output = in.readLine()) != null) {
+                    response.append(output);
+                }
+                in.close();
+
+                //printing result from response
+                System.out.println(response.toString());
+                con.disconnect();
+            }
+            catch (Exception e){
+                System.err.println(e);
+            }
+        }).start();
+    }
+
     /**
      * Open the url in a browser.
      *
@@ -46,30 +94,14 @@ public class OpenUrlAction {
         LOGGER.debug("Open the url: {}", url);
 
         if(helpConfiguration.getHelpViewer() == HelpViewer.EDC_DESKTOP_VIEWER){
+            if(helpConfiguration.getViewerDesktopPath().isEmpty()){
+                LOGGER.error("The path of the application must be entered");
+            }else {
+                setTimeout(() -> System.out.println("test"), 12000, url);
 
-            try
-            {
-                if(helpConfiguration.getViewerDesktopPath().isEmpty()){
-                    LOGGER.error("The path of the application must be entered");
-                }else{
-                    Process process = new ProcessBuilder(helpConfiguration.getViewerDesktopPath(), "edchelp", "fr.edc.ide.eclipse.help").start();
-                    InputStream is = process.getInputStream();
-                    InputStreamReader isr = new InputStreamReader(is);
-                    BufferedReader br = new BufferedReader(isr);
-                    String line;
-
-
-
-                    while ((line = br.readLine()) != null) {
-                        System.out.println(line);
-                    }
+                    Runtime.getRuntime().exec(helpConfiguration.getViewerDesktopPath(), null, null);
 
                     LOGGER.info("Desktop viewer is running");
-                }
-            }
-            catch (IOException e)
-            {
-                LOGGER.error(String.valueOf(e));
             }
         }else if(helpConfiguration.getHelpViewer() == HelpViewer.EMBEDDED_VIEWER){
             browser.setSize(helpConfiguration.getWidthBrowser(), helpConfiguration.getHeightBrowser());
